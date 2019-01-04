46052dc9d07090226175ea7a8de8e6b2c64e7dde
david <david@davids-iMac-Pro.domain.name>
2019-01-04 18:49:24 +0800

    no message

---
+++ WonderAlbum/WonderAlbum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WonderAlbum
+//
+//  Created by david on 2019/1/4.
+//  Copyright © 2019 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WonderAlbum/WonderAlbum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WonderAlbum
+//
+//  Created by david on 2019/1/4.
+//  Copyright © 2019 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
